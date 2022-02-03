aa896216d1c88150ce9362e5e3a78c3bed83cf4e
kzehnter <k.zehnter@reply.de>
2022-02-04 00:32:28 +0100

    refactor for collisiom detection done

--- Ball.cpp
+++ Ball.cpp
@@ -6,6 +6,8 @@
 #include <typeinfo>
 #include <iostream>
 
+const int VERTICES_PER_POLYGON = 3;
+
 Ball::Ball() : GameObject{ glm::vec3{0.0f,0.0f,-10.0f}, glm::vec3{0.5f}, "ball.obj", "mandrill.bmp"}
 {
 	setVelocity(glm::normalize(glm::vec3{ 0.2f, 0.3f, 0.2f }) * 0.3f);
@@ -15,8 +17,23 @@ void Ball::setVelocity(glm::vec3 velocity) {
 	m_velocity = velocity;
 }
 
-static float lengthSquared(glm::vec3 v) {
-	return powf(v.x, 2) + powf(v.y, 2) + powf(v.z, 2);
+void Ball::calcWorldPositionForVertices(glm::vec3  planeVertices[3], GameObject* obj)
+{
+	planeVertices[0] *= obj->getScale();
+	planeVertices[1] *= obj->getScale();
+	planeVertices[2] *= obj->getScale();
+	planeVertices[0] += obj->getPosition();
+	planeVertices[1] += obj->getPosition();
+	planeVertices[2] += obj->getPosition();
+}
+
+bool Ball::doesBallTouchPlane(glm::vec3  planeVertices[3], glm::vec3& vertexNormal)
+{
+	glm::vec3 vertex1ToBall = getPosition() - planeVertices[0];
+	float distance = -glm::dot(vertex1ToBall, vertexNormal);
+	bool outsidePlane = false;
+	float radius = getScale().x / 2;
+	return fabs(distance) <= radius;
 }
 
 static bool leftOf(const glm::vec2& a, const glm::vec2& b, const glm::vec2& p)
@@ -27,65 +44,60 @@ static bool leftOf(const glm::vec2& a, const glm::vec2& b, const glm::vec2& p)
 	return (area > -0.5f);
 }
 
-void Ball::handleCollisions(GameObject* obj, bool& retflag)
+bool Ball::doesBallTouchPolygon(glm::vec3  planeVertices[3], glm::vec3& vertexNormal)
 {
+	glm::vec3 edgeOfPlane = planeVertices[1] - planeVertices[0];
+	glm::vec3 planeX{ glm::normalize(edgeOfPlane) };
+	glm::vec3 planeY{ glm::normalize(glm::cross(vertexNormal, edgeOfPlane)) };
+
+	auto project2D = [&](const glm::vec3& p) {
+		return glm::vec2{ glm::dot(p, planeX), glm::dot(p, planeY) };
+	};
+
+	glm::vec2 v1_2D{ project2D(planeVertices[0]) };
+	glm::vec2 v2_2D{ project2D(planeVertices[1]) };
+	glm::vec2 v3_2D{ project2D(planeVertices[2]) };
+	glm::vec2 ball_2D{ project2D(getPosition()) };
+
+	return leftOf(v1_2D, v2_2D, ball_2D) && leftOf(v2_2D, v3_2D, ball_2D) && leftOf(v3_2D, v1_2D, ball_2D);
+}
+
+void Ball::reboundBall(GameObject* obj, glm::vec3& vertexNormal)
+{
+	if (typeid(*obj) == typeid(Paddle)) {
+		glm::vec3 newBallDirection = getPosition() - (obj->getPosition() - glm::vec3(0, 0, 1));
+		setVelocity(glm::normalize(newBallDirection) * 0.3f);
+	}
+	else {
+		setVelocity(getVelocity() - 2 * glm::dot(getVelocity(), vertexNormal) * vertexNormal);
+	}
+	if (typeid(*obj) == typeid(Brick)) {
+		SceneManager::getInstance().scheduleRemoveGameObject(obj);
+	}
+	return;
+}
+
+void Ball::copyVertices(glm::vec3  planeVertices[3], const std::vector<glm::vec3>& brickVertices, int i)
+{
+	planeVertices[0] = brickVertices[i];
+	planeVertices[1] = brickVertices[i + 1];
+	planeVertices[2] = brickVertices[i + 2];
+}
+
+void Ball::handleCollisions(GameObject* obj, bool& retflag) {
 	retflag = true;
 	const auto brickVertices = obj->getMeshModel().getVertices();
 	const auto brickNormals = obj->getMeshModel().getNormals();
+	
+	for (int i = 0; i <  brickVertices.size(); i+=VERTICES_PER_POLYGON) {
+		glm::vec3 planeVertices[VERTICES_PER_POLYGON];
+		glm::vec3 vertexNormal = glm::normalize(brickNormals[i]);
+		
+		copyVertices(planeVertices, brickVertices, i);
+		calcWorldPositionForVertices(planeVertices, obj);
 
-	int faceCount = brickVertices.size() / 3;
-
-	for (int i = 0; i < faceCount; i++) {
-		bool outsidePlane = false;
-		bool outsideAllVerts = false;
-		bool outsideAllEdges = false;
-		bool fullyInsidePlane = false;
-
-		glm::vec3 v1{ brickVertices[i * 3] };
-		glm::vec3 v2{ brickVertices[i * 3 + 1] };
-		glm::vec3 v3{ brickVertices[i * 3 + 2] };
-
-		// model -> world
-		// object rotation is ignored for now
-		v1 *= obj->getScale();
-		v2 *= obj->getScale();
-		v3 *= obj->getScale();
-		v1 += obj->getPosition();
-		v2 += obj->getPosition();
-		v3 += obj->getPosition();
-		// in this case all three vertex normals for face are parallel so just take the first one
-		glm::vec3 vn = glm::normalize(brickNormals[i * 3]);
-
-		float distance = -glm::dot(getPosition() - v1, vn);
-		// scale is same across all axis for ball -> use any
-		float radius = getScale().x / 2;
-		outsidePlane = fabs(distance) > radius;
-		if (outsidePlane) continue;
-
-		glm::vec3 planeX{ glm::normalize(v2 - v1) };
-		glm::vec3 planeY{ glm::normalize(glm::cross(vn, v2 - v1)) };
-		auto project2D = [&](const glm::vec3& p) {
-			return glm::vec2{ glm::dot(p, planeX), glm::dot(p, planeY) };
-		};
-
-		glm::vec2 v1_2D{ project2D(v1) };
-		glm::vec2 v2_2D{ project2D(v2) };
-		glm::vec2 v3_2D{ project2D(v3) };
-		glm::vec2 ball_2D{ project2D(getPosition())};
-
-		bool isBallInsidePolygon = leftOf(v1_2D, v2_2D, ball_2D) && leftOf(v2_2D, v3_2D, ball_2D) && leftOf(v3_2D, v1_2D, ball_2D);
-		if (isBallInsidePolygon) {
-			if (typeid(*obj) == typeid(Paddle)) {
-				glm::vec3 newBallDirection = getPosition() - (obj->getPosition() - glm::vec3(0, 0, 1));
-				setVelocity(glm::normalize(newBallDirection) * 0.3f);
-			} else {
-				setVelocity(getVelocity() - 2 * glm::dot(getVelocity(), vn) * vn);
-			}
-			if (typeid(*obj) == typeid(Brick)) {
-				SceneManager::getInstance().scheduleRemoveGameObject(obj);
-			}
-			return;
-		}
+		if (!doesBallTouchPlane(planeVertices, vertexNormal)) continue;
+		if (doesBallTouchPolygon(planeVertices, vertexNormal)) return reboundBall(obj, vertexNormal);
 	}
 	retflag = false;
 }
@@ -95,7 +107,6 @@ void Ball::update() {
 	rotate(5, getVelocity());
 
 	for (GameObject* obj : SceneManager::getInstance().getAllSceneObjects()) {
-		// exclude self from scene objects
 		if (typeid(*obj) == typeid(Ball))
 			continue;
 
--- Ball.hpp
+++ Ball.hpp
@@ -6,6 +6,11 @@ class Ball : public GameObject
 {
 private:
 	glm::vec3 m_velocity{};
+	void calcWorldPositionForVertices(glm::vec3  planeVertices[3], GameObject* obj);
+	bool doesBallTouchPlane(glm::vec3  planeVertices[3], glm::vec3& vertexNormal);
+	bool doesBallTouchPolygon(glm::vec3  planeVertices[3], glm::vec3& vertexNormal);
+	void reboundBall(GameObject* obj, glm::vec3& vertexNormal);
+	void copyVertices(glm::vec3  planeVertices[3], const std::vector<glm::vec3>& brickVertices, int i);
 	void handleCollisions(GameObject* obj, bool& retflag);
 public:
 	Ball();
